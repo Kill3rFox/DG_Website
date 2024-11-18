7e2ebf0212ce57ab6d4706d3b6855f36659483e0
Kill3rFox <110303576+Kill3rFox@users.noreply.github.com>
2024-11-18 15:21:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+  function scrollToSection(sectionId) {
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+
